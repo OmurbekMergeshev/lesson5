4b363cf5e01c04006712b5522a5affd20bce759f
Omar <vip.mergeshev@gmail.com>
2023-02-25 23:09:05 +0600

    add lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
 
-function App() {
+export default function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("FirstName", { required: true, maxLength: 20 })} />
+      <input {...register("LastName", { pattern: /^[A-Za-z]+$/i })} />
+      <input type="number" {...register("Age", { min: 18, max: 99 })} />
+      <input type="submit" />
+    </form>
+  )
 }
-
-export default App;
